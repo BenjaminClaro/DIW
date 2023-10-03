14ca7c1bfb188093ee0f3b506fcfd5b389b9f1e1
BenjaminClaro <benjamin.claro@free.fr>
2023-10-03 09:08:43 +0200

    modified:   The District/html/categorie.html 	modified:   The District/html/commande.html 	modified:   The District/html/contact.html 	modified:   The District/html/index.html 	modified:   The District/html/tousplat.html 	modified:   The District/js/javascript.js 	new file:   The District/php/footer.php 	new file:   The District/php/header.php

--- The District/js/javascript.js
+++ The District/js/javascript.js
@@ -1,1 +1,21 @@
-$(document).ready
+$(document).ready(function()
+{
+
+    $.ajax({
+        url: "../php/header.php",
+        type: "GET",
+        success: function(data){
+            $("#header").html(data);
+        }
+    });
+
+    $.ajax({
+        url: "../php/footer.php",
+        type: "GET",
+        success: function(data){
+            $("#footer").html(data);
+        }
+    });
+
+
+});
---
+++ The District/php/header.php
@@ -0,0 +1,21 @@
+<header class="navbar navbar-expand-sm navbar-light" style="background-color : #970747;">
+        <div class="container-fluid ">
+            <div class="row ">
+                <div class="col-1"><img src="../assets/img/images_the_district/the_district_brand/logo.png" alt="logo" class="img-fluid"></div>
+                <div class="col-3 text-right font-weight-bold h1 mt-4 d-none d-md-block"><a href="index.html" style="color :#fef4e8">Acceuil</a></div>
+                <div class="col-2 text-right font-weight-bold h1 mt-4 d-none d-md-block"><a href="categorie.html" style="color :#fef4e8">Catégorie</a></div>
+                <div class="col-2 text-right font-weight-bold h1 mt-4 d-none d-md-block"><a href="tousplat.html" style="color :#fef4e8">Plats</a></div>
+                <div class="col-2 text-right font-weight-bold h1 mt-4 d-none d-md-block"><a href="contact.html" style="color :#fef4e8">Contact</a></div>  
+            </div>
+            <div class="dropdown justify-content-center mt-3 d-sm-block d-md-none">
+                <button type="button" class="btn btn-dark dropdown-toggle" data-toggle="dropdown">Menu </button>
+                <div class="dropdown-menu dropdown-menu-right ">
+                  <a class="dropdown-item" href="index.html">Acceuil</a>
+                  <a class="dropdown-item" href="categorie.html">Catégorie</a>
+                  <a class="dropdown-item" href="tousplat.html">Plats</a>
+                  <a class="dropdown-item" href="contact.html">Contact</a>
+                </div>
+            </div>
+        </div>
+        
+</header>
