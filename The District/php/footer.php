7a523c1cf0d07ca5db50c4854d60beb019d254e7
BenjaminClaro <benjamin.claro@free.fr>
2023-10-09 08:39:03 +0200

    new file:   The District/assets/img/images_the_district/the_district_brand/Facebook_Logo_(2019).png 	new file:   The District/assets/img/images_the_district/the_district_brand/Instagram_icon.png 	new file:   The District/assets/img/images_the_district/the_district_brand/Logo_of_Twitter.png 	modified:   The District/php/footer.php

--- The District/php/footer.php
+++ The District/php/footer.php
@@ -5,6 +5,13 @@
 
                     <div class="col-2"><img src="../assets/img/images_the_district/the_district_brand/favicon.png" class="image-fluid"></div>
 
+                    <div class="col-2"></div>
+
+                    <div class="col-1"> <a href="https://twitter.com/?lang=fr"><img src="../assets/img/images_the_district/the_district_brand/Logo_of_Twitter.png" class="image-fluid"></a> </div>
+                    <div class="col-1"> <a href="https://www.facebook.com/?locale=fr_FR"><img src="../assets/img/images_the_district/the_district_brand/Facebook_Logo_(2019).png" class="image-fluid"></a> </div>
+                    <div class="col-1"> <a href="https://www.instagram.com/"><img src="../assets/img/images_the_district/the_district_brand/Instagram_icon.png" class="image-fluid"></a> </div>
+
+
                 </div>
             </div>
         
